77b4a77fc14c8453f67426bce9e8cfc71cd0838d
Bryant Lopez <bmiguel0170@gmail.com>
2024-04-22 15:40:11 +0000

    Started script

--- salon.sh
+++ salon.sh
@@ -1,1 +1,12 @@
 #! /bin/bash
+PSQL="psql --username=freecodecamp --dbname=salon -c"
+
+MAIN_MENU () {
+  echo $1
+  # display services
+}
+
+echo -e "\n\n~~~~~ MY SALON ~~~~~\n"
+MAIN_MENU "Welcome to My Salon, how can I help you?"
+SERVICES=$($PSQL "SELECT service_id, name FROM services")
+echo "$SERVICES"
